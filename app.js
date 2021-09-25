f6dad082504b2d92c3e7c4380e3e7649e1423294
Ashvini Praisoody <p.aishuash@gmail.com>
2021-09-25 21:56:27 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,67 @@
-const express = require('express')
+const fs = require("fs");
+const express = require("express");
+const multer = require("multer");
+const OAuth2Data = require("./credentials.json");
+var name,pic
 
-const app = express()
+const { google } = require("googleapis");
 
-app.set('view engine','ejs')
+const app = express();
 
-app.get('/',(req,res) =>{
-    res.render("index")
+
+const CLIENT_ID = OAuth2Data.web.client_id;
+const CLIENT_SECRET = OAuth2Data.web.client_secret;
+const REDIRECT_URL = OAuth2Data.web.redirect_uris[0];
+
+const oAuth2Client = new google.auth.OAuth2(
+  CLIENT_ID,
+  CLIENT_SECRET,
+  REDIRECT_URL
+);
+var authed = false;
+
+const SCOPES =
+  "https://www.googleapis.com/auth/userinfo.profile";
+  
+app.set("view engine", "ejs");
+
+
+app.get("/", (req, res) => {
+  if (!authed) {
+    var url = oAuth2Client.generateAuthUrl({
+      access_type: "offline",
+      scope: SCOPES,
+    });
+    console.log(url);
+    res.render("index", { url: url });
+  } 
+});
+
+app.get('/logout',(req,res) => {
+    authed = false
+    res.redirect('/')
 })
 
-app.listen(5000,() => {
-    console.log('App started on Port 5000')
-})
+app.get("/google/callback", function (req, res) {
+  const code = req.query.code;
+  if (code) {
+    oAuth2Client.getToken(code, function (err, tokens) {
+      if (err) {
+        console.log("Error authenticating");
+        console.log(err);
+      } else {
+        console.log("Successfully authenticated");
+        console.log(tokens)
+        oAuth2Client.setCredentials(tokens);
+
+
+        authed = true;
+        res.redirect("/");
+      }
+    });
+  }
+});
+
+app.listen(5000, () => {
+  console.log("App is listening on Port 5000");
+});
